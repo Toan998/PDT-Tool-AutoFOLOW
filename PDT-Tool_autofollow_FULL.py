e9d4a41e2cd9a498bc9c3568c23d2d459b6bdd24
Toan998 <toanphamduc2222@gmail.com>
2025-07-01 22:52:32 +0700

    Update PDT-Tool_autofollow_FULL.py

--- PDT-Tool_autofollow_FULL.py
+++ PDT-Tool_autofollow_FULL.py
@@ -160,7 +160,7 @@ def run_job_mode(auto_follow=True, total_jobs=5, delay=6):
             if auto_follow:
                 adb_auto_follow()
 
-                    res = confirm_job_done(job['job_id'], auth)
+            res = confirm_job_done(job['job_id'], auth)
 if res['status'] == "success":
     money = res.get("money", 0)
     pending = get_pending_money()
