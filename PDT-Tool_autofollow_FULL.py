0276382931f50bbce72b0301862a41edea23c749
Toan998 <toanphamduc2222@gmail.com>
2025-07-01 22:35:40 +0700

    Update PDT-Tool_autofollow_FULL.py

--- PDT-Tool_autofollow_FULL.py
+++ PDT-Tool_autofollow_FULL.py
@@ -133,10 +133,8 @@ def run_job_mode(auto_follow=True, total_jobs=5, delay=6):
     acc = get_linked_tiktok_id(auth)
     print(f"🔐 Mã Authorization: {auth}")
     print(f"📱 TikTok ID đã liên kết: {acc}\\n")
-")
     print(f"⚙️ Đang chạy {total_jobs} job | Delay mỗi job: {delay} giây")
-    print(f"{YELLOW}⛔ Nhấn Ctrl+C để dừng tool bất kỳ lúc nào...{RESET}
-")
+    print(f"{YELLOW}⛔ Nhấn Ctrl+C để dừng tool bất kỳ lúc nào...{RESET}")
 
     try:
         for i in range(1, total_jobs + 1):
